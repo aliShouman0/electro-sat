dc1872b8e2ed8b9a10672592fa9f5149b3cc6fb5
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-07 11:51:00 +0200

    Adding navigation to Dashboard page

--- electro-sat/src/App.js
+++ electro-sat/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<Login />} /> 
+      <Route path={"/dashboard"} element={<Dashboard />} /> 
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
