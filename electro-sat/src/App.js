ad7b2d89a70c2ea53e52293b423cb271ef626fb1
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-08 20:37:04 +0200

    Frontend:Adding path for Daily page

--- electro-sat/src/App.js
+++ electro-sat/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Daily from "./pages/Daily/Daily";
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path={"/"} element={<Login />} /> 
+      <Route path={"/login"} element={<Login />} /> 
       <Route path={"/dashboard"} element={<Dashboard />} /> 
+      <Route path={"/daily"} element={<Daily  />} /> 
 
     </Routes>
   </BrowserRouter>
