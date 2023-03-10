f6820077d66338e9477ece0364491eef2a189603
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-10 19:19:41 +0200

    Frontend: Adding titile and fixing styles

--- electro-sat/src/pages/Daily/Daily.jsx
+++ electro-sat/src/pages/Daily/Daily.jsx
@@ -8,7 +8,7 @@ function Daily() {
     <>
       <NavBar userName={"علي شومان"} userImg={userImg} />
       <SideBar />
-      <main className="fixed  top-28 w-5/6 flex  justify-center  right-40   overflow-y-auto  ">
+      <main className="fixed  top-28 w-full  flex  flex-col items-center  justify-center  right-24  overflow-y-auto  ">
         <div className="top-bar bg-greyBar h-14  py-1 w-3/4 rounded-lg flex items-center justify-end font-bold">
           <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
             صرف
@@ -19,6 +19,12 @@ function Daily() {
           <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
             مبيع
           </button>
+        </div>
+        <div className="sales mt-5 w-full ">
+          <div className="title w-1/2 bg-secondary text-center m-auto py-1 rounded-xl">
+            <p>المبيعات</p>
+          </div>
+          
         </div>
       </main>
     </>
