f90330cc4d7de2c4fd4dd6ca7986cbf8070e9dc2
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-10 18:59:24 +0200

    Frontend: Creating and styling top bar

--- electro-sat/src/pages/Daily/Daily.jsx
+++ electro-sat/src/pages/Daily/Daily.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import userImg from "../../assets/images/userImg.png";
+import SideBar from "../../components/SideBar/SideBar";
 
 function Daily() {
   return (
-    <div>Daily</div>
-  )
+    <>
+      <NavBar userName={"علي شومان"} userImg={userImg} />
+      <SideBar />
+      <main className="fixed  top-28 w-5/6 flex  justify-center  right-40   overflow-y-auto  ">
+        <div className="top-bar bg-greyBar h-14  py-1 w-3/4 rounded-lg flex items-center justify-end font-bold">
+          <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
+            صرف
+          </button>
+          <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
+            دفع دائن
+          </button>
+          <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
+            مبيع
+          </button>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default Daily
+export default Daily;
