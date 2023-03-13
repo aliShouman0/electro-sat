60f42dc6ede2f05314fbf63facf2d9b2ef73ceb3
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-13 18:51:30 +0200

    Frontend: Adding Category

--- electro-sat/src/pages/Stock/Stock.jsx
+++ electro-sat/src/pages/Stock/Stock.jsx
@@ -3,13 +3,23 @@ import NavBar from "../../components/NavBar/NavBar";
 import SideBar from "../../components/SideBar/SideBar";
 
 import userImg from "../../assets/images/userImg.png";
+import Category from "../../components/Category/Category";
+import { faPlug, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function Stock() {
   return (
     <>
       <NavBar userName={"علي شومان"} userImg={userImg} />
       <SideBar />
-      <main className="fixed   w-[80%] h-full flex flex-col items-center  overflow-y-auto overflow-x-hidden    top-28  right-44"></main>
+      <main className="fixed  w-[80%] h-full overflow-y-auto overflow-x-hidden    top-28  right-44">
+        <div className="flex justify-start   gap-5 flex-wrap ">
+          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+        </div>
+      </main>
     </>
   );
 }
