4a95ab2949f6bf0e7d870cf8c1508f4412f6cfb1
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-13 18:56:56 +0200

    Frontend: Editing  Categories data

--- electro-sat/src/pages/Stock/Stock.jsx
+++ electro-sat/src/pages/Stock/Stock.jsx
@@ -4,7 +4,7 @@ import SideBar from "../../components/SideBar/SideBar";
 
 import userImg from "../../assets/images/userImg.png";
 import Category from "../../components/Category/Category";
-import { faPlug, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCamera, faLaptop, faPhone, faPlug, faSatellite, faSatelliteDish, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function Stock() {
   return (
@@ -12,12 +12,12 @@ function Stock() {
       <NavBar userName={"علي شومان"} userImg={userImg} />
       <SideBar />
       <main className="fixed  w-[80%] h-full overflow-y-auto overflow-x-hidden    top-28  right-44">
-        <div className="flex justify-start   gap-5 flex-wrap ">
+        <div className="flex justify-start   gap-4 flex-wrap ">
           <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
-          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
-          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
-          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
-          <Category title={"كهرباء"} products={14} pieces={240} icon={faPlug} />
+          <Category title={"هواتف"} products={64} pieces={540} icon={faPhone} />
+          <Category title={"ستالايت"} products={10} pieces={162} icon={faSatelliteDish} />
+          <Category title={"مراقبه"} products={36} pieces={652} icon={faCamera} />
+          <Category title={"كمبيوتر"} products={32} pieces={236} icon={faLaptop} />
         </div>
       </main>
     </>
