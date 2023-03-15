214ce561141b80227d746d7a2e159cb77a84fa8f
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-15 14:34:09 +0200

    Frontend: Adding nav and side bar

--- electro-sat/src/pages/Category/Category.jsx
+++ electro-sat/src/pages/Category/Category.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import SideBar from "../../components/SideBar/SideBar";
+import userImg from "../../assets/images/userImg.png";
+
 
 function Category() {
   const { title } = useParams();
 
-  return <div>Category {title}</div>;
+  return (
+    <>
+      Category {title}
+      <div>
+        <NavBar userName={"علي شومان"} userImg={userImg} />
+        <SideBar />
+      </div>
+    </>
+  );
 }
 
 export default Category;
