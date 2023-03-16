07a8d0396801ceee5275a4e37225097cb27a8866
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-16 13:50:08 +0200

    Frontend:Removing uneeded btn and editing styles

--- electro-sat/src/components/SideBar/SideBar.jsx
+++ electro-sat/src/components/SideBar/SideBar.jsx
@@ -9,56 +9,52 @@ import "./index.css";
 function SideBar() {
   return (
     <div className="fixed bg-primary h-screen  w-40 right-0 top-24 z-0    ">
-      <div className="side-link w-full  font-bold   text-xs ">
-        <NavLink to={"/dashboard"}>
-          <p className=" w-full h-12  mb-3 flex items-center justify-center">
-            لوحة القيادة
-          </p>
-        </NavLink>
-        <NavLink to={"/daily"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            اليوميه
-          </p>
-        </NavLink>
+      <div className="h-5/6 w-full flex flex-col  ">
+        <div className="side-link w-full  font-bold   text-xs ">
+          <NavLink to={"/dashboard"}>
+            <p className=" w-full h-11  mb-3 flex items-center justify-center">
+              لوحة القيادة
+            </p>
+          </NavLink>
+          <NavLink to={"/daily"}>
+            <p className="w-full h-11  mb-3 flex items-center justify-center  hover:opacity-80 ">
+              اليوميه
+            </p>
+          </NavLink>
 
-        <NavLink to={"/stock"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            البضاعه
-          </p>
-        </NavLink>
-
-        <NavLink to={"/expenses"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            المصاريف
-          </p>
-        </NavLink>
-        <NavLink to={"/login"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            الديون
-          </p>
-        </NavLink>
-        <NavLink to={"/login"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            السجلات
-          </p>
-        </NavLink>
-        <NavLink to={"/login"}>
-          <p className="w-full h-12  mb-3 flex items-center justify-center  hover:opacity-80 ">
-            الموردون
-          </p>
-        </NavLink>
-      </div>
-      <div className="">
-        <USDInfoBox />
-        <div className="setting mt-3 flex items-center justify-center  cursor-pointer ">
-          <button className="text-sm text-white hover:opacity-80">
-            الاعدادات
-          </button>
-          <FontAwesomeIcon
-            icon={faGear}
-            className=" text-secondary ml-3 hover:animate-spin "
-            size="2xl"
-          />
+          <NavLink to={"/stock"}>
+            <p className="w-full h-11  mb-3 flex items-center justify-center  hover:opacity-80 ">
+              البضاعه
+            </p>
+          </NavLink>
+          <NavLink to={"/login"}>
+            <p className="w-full h-11  mb-3 flex items-center justify-center  hover:opacity-80 ">
+              الديون
+            </p>
+          </NavLink>
+          <NavLink to={"/login"}>
+            <p className="w-full h-11  mb-3 flex items-center justify-center  hover:opacity-80 ">
+              السجلات
+            </p>
+          </NavLink>
+          <NavLink to={"/login"}>
+            <p className="w-full h-11  mb-3 flex items-center justify-center  hover:opacity-80 ">
+              الموردون
+            </p>
+          </NavLink>
+        </div>
+        <div className="mt-auto ">
+          <USDInfoBox />
+          <div className="setting mt-3 flex items-center justify-center  cursor-pointer ">
+            <button className="text-sm text-white hover:opacity-80">
+              الاعدادات
+            </button>
+            <FontAwesomeIcon
+              icon={faGear}
+              className=" text-secondary ml-3 hover:animate-spin "
+              size="2xl"
+            />
+          </div>
         </div>
       </div>
     </div>
