8f6a55c3cb8308227d31a57bb58816fd815b02f4
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-16 11:18:59 +0200

    Frontend: Adding category table

--- electro-sat/src/pages/Category/Category.jsx
+++ electro-sat/src/pages/Category/Category.jsx
@@ -7,7 +7,129 @@ import { DataGrid } from "@mui/x-data-grid";
 
 function Category() {
   const { title } = useParams();
- 
+  const [selectionModel, setSelectionModel] = useState([]);
+  const columns = [
+    {
+      field: "selling_price",
+      headerName: "مبيع",
+      type: "string",
+      width: 60,
+      editable: true,
+      align: "center",
+      headerAlign: "center",
+      border: 1,
+    },
+    {
+      field: "Wholesale_price",
+      headerName: "جمله",
+      type: "string",
+      width: 60,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "bill_number",
+      headerName: "فاتوره رقم",
+      type: "string",
+      width: 80,
+      editable: true,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "supplier",
+      headerName: "المورد",
+      type: "string",
+      width: 60,
+      editable: true,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "quantity",
+      headerName: "الكميه",
+      type: "number",
+      width: 60,
+      editable: true,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "product_name",
+      headerName: "اسم المنتج",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "product_id",
+      headerName: "رقم المنتج",
+      type: "number",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "user_name",
+      headerName: "اسم المسخدم",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "id",
+      headerName: "ID",
+      width: 15,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+  ];
+
+  const rows = [
+    {
+      selling_price: "780$",
+      Wholesale_price: "150$",
+      bill_number: 18974,
+      supplier: "Lynn",
+      quantity: 159,
+      product_name: "USB",
+      product_id: 1234,
+      user_name: "علي",
+      id: 1,
+    },
+    {
+      selling_price: "780$",
+      Wholesale_price: "150$",
+      bill_number: 18974,
+      supplier: "Lynn",
+      quantity: 159,
+      product_name: "USB",
+      product_id: 1234,
+      user_name: "علي",
+      id: 2,
+    },
+    {
+      selling_price: "780$",
+      Wholesale_price: "150$",
+      bill_number: 18974,
+      supplier: "Lynn",
+      quantity: 159,
+      product_name: "USB",
+      product_id: 1234,
+      user_name: "علي",
+      id: 3,
+    },
+  ];
+  const handleSelectionModelChange = (newSelection) => {
+    setSelectionModel(newSelection.selectionModel);
+  };
 
   return (
     <>
@@ -23,7 +145,24 @@ function Category() {
           </button>
         </div>
 
-     
+        <div className="flex justify-end  gap-4 flex-wrap h-1/2 w-full">
+          <div className="w-auto h-full  m-auto">
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[15, 25, 50]}
+              rowHeight={40}
+              checkboxSelection={false}
+              selectionModel={selectionModel}
+              onSelectionModelChange={handleSelectionModelChange}
+              sx={{
+                boxShadow: 1,
+                fontSize: 14,
+              }}
+            />
+          </div>
+        </div>
       </main>
     </>
   );
