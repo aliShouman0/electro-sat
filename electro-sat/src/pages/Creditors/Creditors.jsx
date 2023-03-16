55b3df0e2a75fd6f3ecc560e94f714b29c013561
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-16 15:02:16 +0200

    Frontend: Adding nav and side bar

--- electro-sat/src/pages/Creditors/Creditors.jsx
+++ electro-sat/src/pages/Creditors/Creditors.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+
+import NavBar from "../../components/NavBar/NavBar";
+import SideBar from "../../components/SideBar/SideBar";
+import userImg from "../../assets/images/userImg.png";
 
 function Creditors() {
   return (
-    <div>Creditors</div>
-  )
+    <>
+      <NavBar userName={"علي شومان"} userImg={userImg} />
+      <SideBar />
+      <main className="fixed   w-[80%] h-full flex flex-col items-end  overflow-y-auto overflow-x-hidden    top-28  right-44 text-white font-bold"></main>
+    </>
+  );
 }
 
-export default Creditors
+export default Creditors;
