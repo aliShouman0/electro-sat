b0a3db299eb8de44bb5797402c58b8bdd5c102c8
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-17 16:12:06 +0200

    Frontend: Finalizing genral style for creditors page

--- electro-sat/src/pages/Creditors/Creditors.jsx
+++ electro-sat/src/pages/Creditors/Creditors.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
 import NavBar from "../../components/NavBar/NavBar";
 import SideBar from "../../components/SideBar/SideBar";
 import userImg from "../../assets/images/userImg.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons"; 
+import {
+  faArrowDown,
+  faArrowUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Creditors() {
   const search = (e) => {
@@ -15,35 +18,36 @@ function Creditors() {
     <>
       <NavBar userName={"علي شومان"} userImg={userImg} />
       <SideBar />
-      <main className="fixed   w-[80%] h-full flex flex-col items-end  overflow-y-auto overflow-x-hidden    top-28  right-44 text-white font-bold">
-        <div className="top-bar bg-greyBar h-14 py-2 w-5/6  rounded-lg flex items-center  justify-end font-bold  ">
-          <form onSubmit={search}>
-            <button
-              type="submit"
-              className="bg-secondary mr-4 py-2 px-2   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-24   "
-            >
-              <FontAwesomeIcon icon={faSearch} className="mr-2" />
-              بحث
-            </button>
 
-            <input
-              type="date"
-              className="bg-secondary mr-4 py-2 px-2  text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
-            />
+      <div className="fixed  top-bar bg-greyBar h-14 py-2 w-3/4  rounded-lg flex items-center  justify-end font-bold top-28  right-44 text-white ">
+        <form onSubmit={search}>
+          <button
+            type="submit"
+            className="bg-secondary mr-4 py-2 px-2   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-24   "
+          >
+            <FontAwesomeIcon icon={faSearch} className="mr-2" />
+            بحث
+          </button>
 
-            <input
-              type="number"
-              className="bg-secondary mr-4 py-2 px-2   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
-              placeholder="رقم الهاتف"
-            />
-            <input
-              type="text"
-              className="bg-secondary mr-4 py-2 px-2  text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
-              placeholder="الاسم"
-            />
-          </form>
-        </div>
-      </main>
+          <input
+            type="date"
+            className="bg-secondary mr-4 py-2 px-2  text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
+          />
+
+          <input
+            type="number"
+            className="bg-secondary mr-4 py-2 px-2   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
+            placeholder="رقم الهاتف"
+          />
+          <input
+            type="text"
+            className="bg-secondary mr-4 py-2 px-2  text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	 outline-none w-40  text-white placeholder:text-white text-right "
+            placeholder="الاسم"
+          />
+        </form>
+      </div>
+
+      <main className="fixed   w-[80%] h-2/3 flex flex-col items-end  overflow-y-auto overflow-x-hidden    top-44  right-44 text-white font-bold"></main>
     </>
   );
 }
