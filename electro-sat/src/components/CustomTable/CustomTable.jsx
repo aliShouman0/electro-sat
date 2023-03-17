6dff0bd105e86a3ad7c88888f57a3ccf77622f53
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-17 16:09:34 +0200

    Frontend:Using mui finalizing the coustomtable

--- electro-sat/src/components/CustomTable/CustomTable.jsx
+++ electro-sat/src/components/CustomTable/CustomTable.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import CustomRow from "../../components/CustomRow/CustomRow";
 
-function CustomTable() {
+function CustomTable({
+  rowTitle,
+  rows,
+  rowValue,
+  nestedTableTitle,
+  nestedRowTitle,
+  nestedRowValue,
+  secondTable,
+  secondTableRowTitle,
+  secondTableRowValue,
+}) {
   return (
-    <div>CustomTable</div>
-  )
+    <TableContainer className=" border my-3 bg-greyBar" component={Paper}>
+      <Table aria-label="collapsible table " className="bg-greyBar">
+        <TableHead className="bg-primary ">
+          <TableRow>
+            <TableCell />
+            {rowTitle.map((val) => (
+              <TableCell align="left" sx={{ color: "white" }}>
+                {val}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <CustomRow
+              key={row.name}
+              row={row}
+              rowValue={rowValue}
+              nestedTableTitle={nestedTableTitle}
+              nestedRowTitle={nestedRowTitle}
+              nestedRowValue={nestedRowValue}
+              secondTable={secondTable}
+              secondTableRowTitle={secondTableRowTitle}
+              secondTableRowValue={secondTableRowValue}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
-export default CustomTable
+export default CustomTable;
