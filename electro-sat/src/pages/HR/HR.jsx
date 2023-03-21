bb416b479923fdf343b458094020345b6422448d
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-21 18:07:07 +0200

    Frontend:Adding table

--- electro-sat/src/pages/HR/HR.jsx
+++ electro-sat/src/pages/HR/HR.jsx
@@ -3,14 +3,72 @@ import {
   faDiagnoses,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DataGrid } from "@mui/x-data-grid";
 import React, { useState } from "react";
 
 function HR() {
   const [superAdmin, setSuperAdmin] = useState(false);
+  const [selectionModel, setSelectionModel] = useState([]);
+
+  const creditorColumns = [
+    {
+      field: "notes",
+      headerName: "ملاحظه",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "rate",
+      headerName: "تقييم",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "phoneNumber",
+      headerName: "رقم الهاتف",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "name",
+      headerName: "اسم",
+      type: "string",
+      width: 80,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "id",
+      headerName: "ID",
+      width: 15,
+      editable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      time: "12:35",
+    },
+  ];
+  const handleSelectionModelChange = (newSelection) => {
+    setSelectionModel(newSelection.selectionModel);
+  };
   return (
     <>
-      {" "}
-      <main className="fixed  w-[80%] h-full overflow-y-auto overflow-x-hidden    top-28  right-44 flex flex-col items-end">
+      <main className="fixed  w-[80%] h-3/4 overflow-y-auto overflow-x-hidden    top-28  right-44 flex flex-col items-end">
         <div className="top-bar bg-greyBar h-14 py-2 w-5/6  rounded-lg flex items-center  justify-end font-bold  mb-3 text-white">
           <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
             الموردون
@@ -28,6 +86,27 @@ function HR() {
               />
             </button>
           )}
+        </div>
+
+        <div
+          className={`creditor w-full  h-3/4 mt-5 flex flex-col items-center  `}
+        >
+          <div className="w-auto  h-full ">
+            <DataGrid
+              rows={rows}
+              columns={creditorColumns}
+              pageSize={5}
+              rowsPerPageOptions={[15, 25, 50]}
+              rowHeight={40}
+              checkboxSelection={false}
+              selectionModel={selectionModel}
+              onSelectionModelChange={handleSelectionModelChange}
+              sx={{
+                boxShadow: 1,
+                fontSize: 14,
+              }}
+            />
+          </div>
         </div>
       </main>
     </>
