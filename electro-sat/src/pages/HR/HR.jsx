f42d7fd683c69e4f1c3fe36be1846ae4f8e9cb96
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-21 17:22:56 +0200

    Frontend:Adding main section and topbar

--- electro-sat/src/pages/HR/HR.jsx
+++ electro-sat/src/pages/HR/HR.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HR() {
-  return <div>HR</div>;
+  return (
+    <>
+      {" "}
+      <main className="fixed  w-[80%] h-full overflow-y-auto overflow-x-hidden    top-28  right-44 flex flex-col items-end">
+        <div className="top-bar bg-greyBar h-14 py-2 w-5/6  rounded-lg flex items-center  justify-end font-bold  mb-3 text-white">
+          <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
+            الموردون
+          </button>
+          <button className="bg-secondary mr-4 py-2 px-6   text-xs  rounded-lg hover:opacity-60 hover:font-semibold hover:text-sm transition-all	    ">
+            العملاء
+          </button>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default HR;
