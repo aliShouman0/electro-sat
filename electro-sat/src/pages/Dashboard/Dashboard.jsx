9aa66b2618db17c246c1e63734729c5a3511f9f6
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-22 21:35:20 +0200

    Frontend:Adding main section

--- electro-sat/src/pages/Dashboard/Dashboard.jsx
+++ electro-sat/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Dashboard() {
-  return <></>;
+  return (
+    <>
+      <main className="fixed  w-[80%] h-5/6 overflow-auto  top-28  right-44 flex flex-wrap ">
+      </main>
+    </>
+  );
 }
 
 export default Dashboard;
