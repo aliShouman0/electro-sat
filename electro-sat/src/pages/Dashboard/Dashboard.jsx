1f815bbdad3445d2735a7a493425ab6957c001c7
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-22 21:37:02 +0200

    Frontend:Adding graph

--- electro-sat/src/pages/Dashboard/Dashboard.jsx
+++ electro-sat/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,502 @@
 import React from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import { ResponsivePie } from "@nivo/pie";
+import { ResponsiveCalendar } from "@nivo/calendar";
+import { calendarData, data, data2 } from "./mockData";
 
 function Dashboard() {
   return (
     <>
       <main className="fixed  w-[80%] h-5/6 overflow-auto  top-28  right-44 flex flex-wrap ">
+        <div className="w-full h-1/3">
+          <ResponsiveCalendar
+            data={calendarData}
+            from="2016-01-01"
+            to="2016-07-12"
+            emptyColor="#eeeeee"
+            colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+            margin={{ top: 0, right: 40, bottom: 0, left: 40 }}
+            yearSpacing={40}
+            monthBorderColor="#ffffff"
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            legends={[
+              {
+                anchor: "bottom-right",
+                direction: "row",
+                translateY: 0,
+                translateX: -80,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 16,
+                itemsSpacing: 4,
+                itemDirection: "right-to-left",
+              },
+            ]}
+          />
+        </div>
+        <div className="h-1/2 w-1/2 mt-10">
+          <ResponsiveBar
+            data={data}
+            keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+            indexBy="country"
+            margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+            padding={0.3}
+            valueScale={{ type: "linear" }}
+            indexScale={{ type: "band", round: true }}
+            colors={{ scheme: "nivo" }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "#38bcb2",
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "#eed312",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              {
+                match: {
+                  id: "fries",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "sandwich",
+                },
+                id: "lines",
+              },
+            ]}
+            borderColor={{
+              from: "color",
+              modifiers: [["darker", 1.6]],
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "country",
+              legendPosition: "middle",
+              legendOffset: 32,
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "food",
+              legendPosition: "middle",
+              legendOffset: -40,
+            }}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{
+              from: "color",
+              modifiers: [["darker", 1.6]],
+            }}
+            legends={[
+              {
+                dataFrom: "keys",
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: "left-to-right",
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+            role="application"
+            ariaLabel="Nivo bar chart demo"
+            barAriaLabel={function (e) {
+              return (
+                e.id + ": " + e.formattedValue + " in country: " + e.indexValue
+              );
+            }}
+          />
+        </div>
+        <div className="h-1/2 w-1/2 mt-10">
+          <ResponsivePie
+            data={data2}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{
+              from: "color",
+              modifiers: [["darker", 0.2]],
+            }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: "color" }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+              from: "color",
+              modifiers: [["darker", 2]],
+            }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              {
+                match: {
+                  id: "ruby",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "c",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "go",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "python",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "scala",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "lisp",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "elixir",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "javascript",
+                },
+                id: "lines",
+              },
+            ]}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+        <div className="h-1/2 w-1/2 mt-10">
+          <ResponsivePie
+            data={data2}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{
+              from: "color",
+              modifiers: [["darker", 0.2]],
+            }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: "color" }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+              from: "color",
+              modifiers: [["darker", 2]],
+            }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              {
+                match: {
+                  id: "ruby",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "c",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "go",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "python",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "scala",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "lisp",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "elixir",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "javascript",
+                },
+                id: "lines",
+              },
+            ]}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+        <div className="h-1/2 w-1/2 mt-10">
+          <ResponsivePie
+            data={data2}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{
+              from: "color",
+              modifiers: [["darker", 0.2]],
+            }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: "color" }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{
+              from: "color",
+              modifiers: [["darker", 2]],
+            }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "rgba(255, 255, 255, 0.3)",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              {
+                match: {
+                  id: "ruby",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "c",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "go",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "python",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "scala",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "lisp",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "elixir",
+                },
+                id: "lines",
+              },
+              {
+                match: {
+                  id: "javascript",
+                },
+                id: "lines",
+              },
+            ]}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
       </main>
     </>
   );
