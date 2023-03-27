087a212f94dfff83f86141fb3130c88b33175aa7
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-27 23:10:13 +0300

    Frontend: Styling the infobox component

--- electro-sat/src/components/InfoBox/InfoBox.jsx
+++ electro-sat/src/components/InfoBox/InfoBox.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
-function InfoBox() {
+function InfoBox({
+  title,
+  oldTotalUSD,
+  todayTotalUDS,
+  oldTotalLira,
+  todayTotalLira,
+}) {
   return (
-    <div>InfoBox</div>
-  )
+    <div className="bg-secondary w-52 h-36 rounded-lg text-center p-1 m-1">
+      <div className="title">
+        <p className="font-bold ">{title}</p>
+      </div>
+      <div className="info relative my-2">
+        <p>
+          {oldTotalLira} L.L + {oldTotalUSD}$
+        </p>
+
+        <p className="font-extrabold text-xl absolute left-4 top-2">+</p>
+        <p>
+          {todayTotalLira} L.L + {todayTotalUDS}$
+        </p>
+        <hr className=" mx-auto my-2  w-3/4 " />
+        <p className="font-bold">
+          {oldTotalLira + todayTotalLira} L.L + {oldTotalUSD + todayTotalUDS}$
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
